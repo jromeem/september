c48213e88f6ba0c7af62117fd0a47db145ff3ab4
Jerome Martinez <jerome.e.martinez@gmail.com>
2018-09-22 00:42:17 -0500

    adding more sym links sigh

--- sept/config/environment.js
+++ sept/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'sept',
     environment,
-    rootURL: '/',
+    rootURL: '/dist/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
